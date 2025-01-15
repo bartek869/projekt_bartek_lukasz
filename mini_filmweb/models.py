2a89c3bb23cc984a99af1677836d844752a8c06a
Lukasz <lukasz@mail.com>
2025-01-15 17:41:22 +0100

    Poprawki w formularzu edycji filmu

--- mini_filmweb/models.py
+++ mini_filmweb/models.py
@@ -1,30 +1,27 @@
 from django.db import models
 
+# Model przechowujący dane o reżyserach
 class Director(models.Model):
-    name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+    name = models.CharField(max_length=100, verbose_name="Imię i nazwisko")
+    birth_date = models.DateField(verbose_name="Data urodzenia")
 
     def __str__(self):
         return self.name
 
-#Director – model przechowujący dane o reżyserach (imię i data urodzenia).
-
+# Model przechowujący dane o filmach
 class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    release_year = models.IntegerField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='movies')
+    title = models.CharField(max_length=200, verbose_name="Tytuł")
+    description = models.TextField(verbose_name="Opis")
+    release_year = models.PositiveIntegerField(verbose_name="Rok wydania")  # Użyłem PositiveIntegerField, aby uniknąć ujemnych lat
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='movies', verbose_name="Reżyser")
+    genres = models.ManyToManyField('Genre', related_name='movies', verbose_name="Gatunki", blank=True)  # Dodano blank=True
 
     def __str__(self):
         return self.title
 
-#Movie – model reprezentujący film, z tytułem, opisem, rokiem wydania i powiązaniem z reżyserem.
-
+# Model przechowujący gatunki filmów
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
-    movies = models.ManyToManyField(Movie, related_name='genres')
+    name = models.CharField(max_length=100, unique=True, verbose_name="Gatunek")  # Dodano unique=True
 
     def __str__(self):
         return self.name
-
-#Genre – model przechowujący gatunki filmów, powiązany wiele do wielu z modelami Movie.
